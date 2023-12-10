1a34824eb7c0544ffcd35d51ade47ea0b963bae1
akmur <akmurzinmihail@gmail.com>
2023-12-10 19:34:54 +0500

    Алгоритм работает для одной прогонки. Попытки запустить цикл.

--- Method_MelderaNida.cpp
+++ Method_MelderaNida.cpp
@@ -39,17 +39,57 @@ VectorXd setInitial_x0(int n);
 
 int set_n();
 
+void print_node(const MatrixXd &node_x);
+
+void print_point(VectorXd &x_central);
+
 void methodNelderaMida() {
     int n = set_n();
     VectorXd x0 = setInitial_x0(n);
     MatrixXd node_x = set_NodeFill(x0);
-    node_x = sort_Node(node_x, n);
-    VectorXd x_central = get_CentralGravied(node_x, n);
-    cout << "Точки:\n"  << node_x << endl;
-    VectorXd x_big=node_x.row(n);
-    VectorXd x_new= display(x_central,x_big);
-    cout << endl << x_big.transpose()<< endl <<  x_central.transpose() << endl<< x_new.transpose();
+    while(1) {
+        node_x = sort_Node(node_x, n);
+        VectorXd x_central = get_CentralGravied(node_x, n);
+        VectorXd x_big = node_x.row(n);
+        VectorXd x_new = display(x_central, x_big);
+        if (x_new(n) < x_big(n)) {
+            node_x.row(n) = x_new.transpose();
+            x_central = get_CentralGravied(node_x, n);
+            VectorXd x_stretching = stretching(x_central, x_new);
+            if (x_stretching(n) < x_new(n)) {
+                node_x.row(n) = x_stretching.transpose();
+                node_x = sort_Node(node_x, n);
+            } else {
+                node_x = sort_Node(node_x, n);
+            }
+            x_big.setZero();
+            x_new.setZero();
+            x_stretching.setZero();
+            x_central.setZero();
+        } else {
+            if (x_new(n) < node_x(n - 1, n)) {
+                node_x.row(n) = x_new.transpose();
+                node_x = sort_Node(node_x, n);
+            } else {
+                VectorXd x_compression = сompression(x_central, x_big);
+                if (x_compression(n) < x_big(n)) {
+                    node_x.row(n) = x_compression.transpose();
+                    node_x = sort_Node(node_x, n);
+                } else {
+                    node_x = reduction(node_x, n);
+                }
+            }
+            x_big.setZero();
+            x_new.setZero();
+            x_central.setZero();
+        }
+        print_node(node_x);
+    }
 }
+
+void print_point(VectorXd &x_central) { cout << "Точка:\n" << x_central.transpose() << endl; }
+
+void print_node(const MatrixXd &node_x) { cout << "Симплекс:\n" << node_x << endl; }
 
 int set_n() {
     int n;
@@ -91,44 +131,44 @@ MatrixXd sort_Node(MatrixXd &node, int n) {
 }
 
 VectorXd display(VectorXd &x_central, VectorXd &x_big) {
-    VectorXd new_point=x_central + alfa * (x_central - x_big);
-    new_point(x_central.size()-1)=foo(new_point);
+    VectorXd new_point = x_central + alfa * (x_central - x_big);
+    new_point(x_central.size() - 1) = foo(new_point);
     return new_point;
 }
 
 VectorXd stretching(VectorXd &x_central, VectorXd &x_display) {
-    VectorXd new_point=x_central + gamma * (x_display - x_central);
-    new_point(x_central.size())=foo(new_point);
+    VectorXd new_point = x_central + gamma * (x_display - x_central);
+    new_point(x_central.size() - 1) = foo(new_point);
     return new_point;
 }
 
 VectorXd сompression(VectorXd &x_central, VectorXd &x_big) {
-    VectorXd new_point=x_central + betta * (x_big - x_central);
-    new_point(x_central.size())=foo(new_point);
+    VectorXd new_point = x_central + betta * (x_big - x_central);
+    new_point(x_central.size() - 1) = foo(new_point);
     return new_point;
 }
 
 MatrixXd reduction(MatrixXd &node, int n) {
     MatrixXd new_node(n + 1, n + 1);
     new_node.setZero();
-    for (int i = 1; i <= n; i++) {
-        new_node.row(i)=node.row(i)+0.5*(node.row(i)-node.row(0));
-        VectorXd new_point=new_node.row(i);
-        new_node(i,n)=foo(new_point);
+    for (int i = 0; i <= n; i++) {
+        new_node.row(i) = node.row(i) + 0.5 * (node.row(i) - node.row(0));
+        VectorXd new_point = new_node.row(i);
+        new_node(i, n) = foo(new_point);
     }
     return new_node;
 }
 
 bool stopping(VectorXd &function, int n) {
-    double value_foo=function.sum()/(n+1);
-    double sigma=0;
-    for (int i=0;i<=n;i++){
-        sigma=(function(i)-value_foo);
+    double value_foo = function.sum() / (n + 1);
+    double sigma = 0;
+    for (int i = 0; i <= n; i++) {
+        sigma = (function(i) - value_foo);
     }
-    sigma= sqrt(sigma/(n+1));
-    if (sigma<epsilon){
+    sigma = sqrt(sigma / (n + 1));
+    if (sigma < epsilon) {
         return 1;
-    }else{
+    } else {
         return 0;
     }
 }
