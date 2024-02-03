2b1eecc653e3488fa85fdfac730d4d9568577ef8
Михаил Акмурзин <akmurzinmihail@gmail.com>
2024-02-03 19:12:40 +0500

    Структурирование проекта

---
+++ src/lib/Method_NewtonAndGauss.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Dense"
+#include <random>
+#include <ctime>
+#include <fstream>
+
+using namespace std;
+using namespace Eigen;
+
+const double epsil = 1e-6;
+const int maxIter = 100;
+const int start = 0, _end = 5;
+const int n_x = 1, b_n = 2, n_indication = 50;
+
+
+VectorXd foo(VectorXd &x, VectorXd &b);
+
+void method_NewtonandGauss();
+
+VectorXd InitialPointY(VectorXd &x, VectorXd &b);
+
+MatrixXd Jacobian(VectorXd &x, VectorXd &b);
+
+void write_file(char *a);
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "windows.h"
+#include "lib/Method_MelderaNida.h"
+#include "lib/Method_NewtonAndGauss.h"
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    method_NewtonandGauss();
+    return 0;
+}
