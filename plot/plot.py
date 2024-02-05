1208b4347e2e1065c8d470da197b1f7336aa4019
Михаил Акмурзин <akmurzinmihail@gmail.com>
2024-02-05 19:51:59 +0500

    Структурирование проекта

--- plot/plot.py
+++ plot/plot.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def plot_point():
-    point = pd.read_csv("../../cmake-build-debug/src/Point.txt", sep=' ', names=['X', 'Y'])
-    line = pd.read_csv("../../cmake-build-debug/src/Line.txt", sep=' ', names=['X', 'Y'])
+    point = pd.read_csv("../cmake-build-debug/example/Point.txt", sep=' ', names=['X', 'Y'])
+    line = pd.read_csv("../cmake-build-debug/example/Line.txt", sep=' ', names=['X', 'Y'])
 
     plt.scatter(point['X'], point['Y'], label='Точки')
     plt.plot(line['X'], line['Y'], label='Прямая')
